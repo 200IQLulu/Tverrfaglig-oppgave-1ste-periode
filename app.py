79711abb3b2e0ef1280536bb6be3a3f19d738fba
200IQLulu <ludvigmegeberg@gmail.com>
2026-01-12 12:42:35 +0100

    jeg la til style.css og index filer, og fjernet all kode fordi hadde en feil som ikke ville bort så jeg bare starter på nytt

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect
-from handleliste import legg_til_vare, hent_liste
-form lagring import last_liste, lagre_liste
-
-app = Flask(__name__)
-
-handleliste = last_liste()
-
-@app.route('/', methods=['GET', 'POST'])
