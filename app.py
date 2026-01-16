ef1dfc490c31ffb95669183215866aa294cb7bb8
200IQLulu <ludvigmegeberg@gmail.com>
2026-01-16 10:51:50 +0100

    jeg lagde css koden til nav baren og passet på å bruke farger som er tydelinge å se og riktig font, jeg bytter kansje farge senere.

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ app = Flask(__name__, template_folder="template", static_folder="static")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='index')
 
 @app.route('/handleliste')
 def handleliste():
-    return render_template('handleliste.html')
+    return render_template('handleliste.html', active_page='handleliste')
 
 @app.route('/kategorier')
 def kategorier():
-    return render_template('kategorier.html')
+    return render_template('kategorier.html', active_page='kategorier')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
